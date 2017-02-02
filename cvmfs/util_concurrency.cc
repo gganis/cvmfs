f57208fe069378fbce5b692bfe16983d2e07b186
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2017-02-02 09:08:08 +0100

    CVM-689: definition of Mutex and Condition

--- cvmfs/util_concurrency.cc
+++ cvmfs/util_concurrency.cc
@@ -42,7 +42,7 @@ Signal::~Signal() {
 
 
 void Signal::Wait() {
-  MutexLockGuard guard(lock_);
+  RAII<pthread_mutex_t> guard(lock_);
   while (!fired_) {
     int retval = pthread_cond_wait(&signal_, &lock_);
     assert(retval == 0);
@@ -51,10 +51,43 @@ void Signal::Wait() {
 
 
 void Signal::Wakeup() {
-  MutexLockGuard guard(lock_);
+  RAII<pthread_mutex_t> guard(lock_);
   fired_ = true;
   int retval = pthread_cond_broadcast(&signal_);
   assert(retval == 0);
+}
+
+//
+// +----------------------------------------------------------------------------
+// |  Condition
+//
+
+int Condition::Wait(int64_t msec) const
+{
+  timespec timeout;
+  timeval now;
+  if (ownmtx_) Lock();
+  gettimeofday(&now, NULL);
+  int64_t nsecs = now.tv_usec * 1000 + (msec % 1000)*1000*1000;
+  int carry = 0;
+  if (nsecs >= 1000*1000*1000) {
+     carry = 1;
+     nsecs -= 1000*1000*1000;
+  }
+  timeout.tv_sec = now.tv_sec + msec/1000 + carry;
+  timeout.tv_nsec = nsecs;
+
+  // Now wait for the condition or timeout
+  int rc = 0;
+  do {
+    rc = pthread_cond_timedwait(&cond_, &mutex_, &timeout);
+  } while (rc && (rc == EINTR));
+
+  if (ownmtx_) Unlock();
+
+  assert(rc == 0 || rc == ETIMEDOUT);
+
+  return rc;
 }
 
 #ifdef CVMFS_NAMESPACE_GUARD
